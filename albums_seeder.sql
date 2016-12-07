f1d8abcf5b598ff0e6a31b14192beddb0bf3fc79
Eunice Heymann <heymann.eunice@gmail.com>
2016-12-07 12:01:58 -0600

    Conducted the following exercise:
    Create a new file named albums_seeder.sql.
    At the top of albums_seeder.sql be sure to USE the codeup_test_db database.
    Use INSERT to add records for all the albums from this list on Wikipedia that claim over 30 million sales (the first two tables). For sales data, use the "sales certification" column of the tables, not "claimed sales". For artists listed with "Various Artists", just use the primary artist's name.
    
        First write your queries as separate INSERT statements for each record and test. You should see no output.
        Refactor your script to use a single INSERT statement for all the records and test it again. Again, this should not generate any output.

--- albums_seeder.sql
+++ albums_seeder.sql
@@ -1,5 +1,8 @@
 USE codeup_test_db;
-INSERT INTO albums(
+
+TRUNCATE albums;
+
+INSERT INTO albums (
   artist,
   name,
   release_date,
@@ -7,18 +10,61 @@ INSERT INTO albums(
   sales_in_millions
 )
 VALUES
-('Michael Jackson', 'Thriller', '1982', 'Pop, Rock, R&B', '46.0'),
+  ('Michael Jackson', 'Thriller', '1982', 'Pop, Rock, R&B', '46.0'),
 
-('AC/DC', 'Back in Black', '1980', 'Hard rock', '26.1'),
+  ('AC/DC', 'Back in Black', '1980', 'Hard rock', '26.1'),
 
-('Pink Floyd', 'The Dark Side of the Moon', '1973', 'Progressive rock', '24.2'),
+  ('Pink Floyd', 'The Dark Side of the Moon', '1973', 'Progressive rock', '24.2'),
 
-('Whitney Houston', 'The Bodyguard', '1992', 'R&B, Soul, Pop, Soundtrack', '27.4'),
+  ('Whitney Houston', 'The Bodyguard', '1992', 'R&B, Soul, Pop, Soundtrack', '27.4'),
 
-('Meat Loaf', 'Bat OUT of Hell', '1977', 'Hard rock, Progressive rock', '20.6'),
+  ('Meat Loaf', 'Bat OUT of Hell', '1977', 'Hard rock, Progressive rock', '20.6'),
 
-('Eagles', 'Their Greatest Hits', '1976', 'Rock, Soft rock, Folk rock ', '32.2'),
+  ('Eagles', 'Their Greatest Hits', '1976', 'Rock, Soft rock, Folk rock ', '32.2'),
 
-('Bee Gees', 'Saturday Night Fever', '1977', 'Disco', '20.6'),
+  ('Bee Gees', 'Saturday Night Fever', '1977', 'Disco', '20.6'),
 
-('Fleetwood Mac', 'Rumours', '1977', 'Soft rock', '27.9');
+  ('Fleetwood Mac', 'Rumours', '1977', 'Soft rock', '27.9'),
+
+  ('Shania Twain', 'Come ON Over', '1997', 'Country Pop', '29.6'),
+
+  ('Michael Jackson', 'Bad', '1987', 'Pop, Funk, Rock', '20.3'),
+
+  ('Alanis Morissette', 'Jagged Little Pill', '1995', 'Alternative rock', '24.8'),
+
+  ('Led Zeppelin', 'Led Zeppelin IV', '1971', 'Hard rock, Heavy metal', '29.0'),
+
+  ('Adele', '21', '2011', 'Pop, Soul', '25.3'),
+
+  ('Celine Dion', 'Falling INTO You', '1996', 'Pop, Soft rock', '20.2'),
+
+  ('The Beatles', 'Sgt. Peppers Lonely Hearts Club Band', '1967', 'Rock', '13.1'),
+
+  ('Eagles', 'Hotel California', '1976', 'Rock, Soft rock, Folk rock', '21.5'),
+
+  ('Mariah Carey', 'Music Box', '1993', 'Pop, R & B, Rock', '19.0'),
+
+  ('Michael Jackson', 'Dangerous', '1991', 'Rock, Funk, Pop', '17.6'),
+
+  ('Various artists', 'Dirty Dancing', '1987', 'Pop, Rock, R & B', '17.9'),
+
+  ('Bruce Springsteen', 'Born in the U.S.A.', '1984', 'Rock', '19.6'),
+
+  ('Dire Straits', 'Brothers in Arms', '1985', 'Rock', '17.7'),
+
+  ('James Horner', 'Titanic:Music from the Motion Picture', '1997', 'Soundtrack', '18.1'),
+
+  ('Madonna', 'The Immaculate Collection', '1990', 'Pop, Dance', '19.4'),
+
+  ('Metallica', 'Metallica', '1991', 'Thrash metal, Heavy metal', '19.9'),
+
+  ('Nirvana', 'Nevermind', '1991', 'Grunge, Alternative rock', '16.7'),
+
+  ('Pink Floyd', 'The Wall', '1979', 'Progressive rock', '17.6'),
+
+  ('Santana', 'Supernatural', '1999', 'Rock', '20.5'),
+
+  ('Guns N Roses', 'Appetite for Destruction', '1987', 'Heavy metal, Hard rock', '21.3');
+
+
+
